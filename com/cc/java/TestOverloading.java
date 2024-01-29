e228e29bac3fb92b38d6367dc724399413f6bf7a
J1991Ha <jasper.haag@icloud.com>
2024-01-29 12:26:06 +0100

    overloading activated

--- com/cc/java/TestOverloading.java
+++ com/cc/java/TestOverloading.java
@@ -16,15 +16,15 @@ public class TestOverloading {
 	
 		 
 	// Gleiche Signatur | return type ?	
-	// public void sayHello() {
-	// 	System.out.println("Hello");
-	// }
+	/* public void sayHello() {
+	 	System.out.println("Hello");
+	 }
 	
-	// public String sayHello() {
-	// 	//System.out.println("Hello");
-	// 	return "hi";
-	// }
-	
+	 public String sayHello() {
+	System.out.println("Hello");
+	 	return "hi";
+	 }
+	*/
 	
 	
 	// Gleiche Signatur 3 | Access Modifier ?
@@ -39,47 +39,47 @@ public class TestOverloading {
 	
 		
 	// Gleiche Signatur 3 | Parameter ?
-	// public void sayHello2(String name, int counter) {
-	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
-	// }
-	
-	// public void sayHello2(String name, int counter) {
-	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
-	// }
+	/* public void sayHello2(String name, int counter) {
+	 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
+	 }
+	 public void sayHello2(String name, int counter) {
+	 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
+	 }*/
 
 
-	// Gleiche Signatur 3 | Parameter Namen ?
-	// public void sayHello2(String name, int counter) {
-	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
-	// }
-	
-	// public void sayHello2(String pString, int pCount) {
-	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
-	// }
+//	 Gleiche Signatur 3 | Parameter Namen ?
+//	 public void sayHello2(String name, int counter) {
+//	 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
+//	 }
+//
+//	 public void sayHello2(String pString, int pCount) {
+//	 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
+//	 }
 	
 	
 	// BasisMethode
 	public void sayHello2(String name, int counter) {
 		System.out.println("1: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
-	
+
 	// // Überladene Methode 1 (Reihenfolge Parameter)
 	public void sayHello2(int counter, String name) {
 		System.out.println("2: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
-	
+
 	// // Überladene Methode 3 (Anzahl Parameter)
 	public void sayHello2() {
 		System.out.println("3: Ohne parameter");
+
 	}
-	
-	
-	// // BasisMethode
+//
+//
+//	// // BasisMethode
 	public void sayHello3(String name, int ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
 	}
-	
-	
+
+
 	// // Überladene Methode 3 (Parameter Typ)
 	public void sayHello3(String name, String ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
